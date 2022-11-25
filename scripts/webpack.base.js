faaafd9367d1d98625417bbab99239b4f2f6c72e
CMJ <chemingjun@kuaishou.com>
2022-11-25 17:10:21 +0800

    init all

---
+++ scripts/webpack.base.js
@@ -0,0 +1,52 @@
+﻿const webpack = require('webpack')
+
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    alias: {
+
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        //include: [pathSrc, testSrc],
+        use: [
+          // {
+          //   loader: './config/test-loader'
+          // },
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-react'  // jsx支持
+                //['@babel/preset-env', { useBuiltIns: 'usage', corejs: 2 }] // 按需使用polyfill
+              ],
+              plugins: [
+                ['@babel/plugin-proposal-class-properties', { 'loose': true }] // class中的箭头函数中的this指向组件
+              ],
+              cacheDirectory: true // 加快编译速度
+            }
+          },
+          {
+            loader: 'ts-loader',
+            options: {
+              silent: true,
+              transpileOnly: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // new webpack.DefinePlugin({
+    //   'process.env': {
+    //     NODE_ENV: JSON.stringify('development')
+    //   }
+    // }),
+
+    //new BundleAnalyzerPlugin()
+  ]
+}
---
+++ scripts/webpack.build.js
@@ -0,0 +1,35 @@
+﻿const path = require('path')
+
+const baseCfg = require('./webpack.base')
+
+module.exports = Object.assign({
+   entry: {
+      index: path.resolve(__dirname, '../src/index.ts')
+   },
+   // optimization: {
+   //   splitChunks: {
+   //     chunks: "all",
+   //     //minSize: 100,
+   //     cacheGroups: {
+   //       vendors: {
+   //         test: /[\\/]node_modules[\\/]/,  // 匹配node_modules目录下的文件
+   //         priority: -10   // 优先级配置项
+   //       },
+   //       default: {
+   //         minChunks: 2,
+   //         priority: -20,   // 优先级配置项
+   //         reuseExistingChunk: true
+   //       }
+   //     }
+   //   }
+   // },
+   output: {
+      path: path.resolve(__dirname, '..'),
+      filename: './index.js',
+      libraryTarget: 'umd',
+      //library: 'rxui',
+   },
+   externals: [{
+   }],
+   //devtool: 'cheap-module-eval-source-map',//devtool: 'cheap-source-map',
+}, baseCfg)
