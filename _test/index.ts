4d3213c4fe4ca5de71402a605feabcf2026b357f
tangxiaoxin <tangxiaoxin@kuaishou.com>
2022-12-13 19:44:41 +0800

    _test

---
+++ _test/index.ts
@@ -0,0 +1,12 @@
+import { getJSONFromRXUIFile } from "../src";
+import { readFileSync, writeFileSync } from "fs";
+import { resolve } from "path";
+
+(async () => {
+  const contentJson = await readFileSync(resolve(__dirname, "content.json"));
+  const resultContent = getJSONFromRXUIFile(JSON.parse(contentJson.toString()));
+  writeFileSync(
+    resolve(__dirname, "result.json"),
+    JSON.stringify(resultContent)
+  );
+})();
